5d5e867a9c32be111c856b710d1eb66dc17c2d9d
sowdri <sowdri@mediaiqdigital.com>
2016-01-14 14:50:05 +0530

    With redux

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,16 @@
-import './css/main.css';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import todoApp from './reducers'
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+let store = createStore(todoApp)
 
-ReactDOM.render(<App />, document.getElementById('app'));
+let rootElement = document.getElementById('app')
+render(
+  <Provider store={ store }>
+    <App />
+  </Provider>,
+  rootElement
+)
